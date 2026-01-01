b1743945eddeb274694094d9cb600ae7ea34898c
Zain0205 <fahmizain000@gmail.com>
2026-01-01 18:08:50 +0700

    feat: create database connection

---
+++ internal/database/mysql.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect(dsn string) {
+	var err error
+
+	for i := 1; i <= 10; i++ {
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			fmt.Println("Database connected")
+			return
+		}
+
+		fmt.Printf("Waiting for MySQL... (%d/10)\n", i)
+		time.Sleep(3 * time.Second)
+	}
+
+	panic("Failed to connect MySQL after retries")
+}
