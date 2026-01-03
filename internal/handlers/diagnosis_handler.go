eca6ec453678646988c6aeaa8dc1b9b67769c1c2
Zain0205 <fahmizain000@gmail.com>
2026-01-03 18:39:10 +0700

    feat: create diagnosis handler

---
+++ internal/handlers/diagnosis_handler.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"github.com/Zain0205/cf-stunting-backend-go/internal/services"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type DiagnosisRequest struct {
+	Category string                 `json:"category"`
+	Answers  []services.AnswerInput `json:"answers"`
+}
+
+func CreateDiagnosis(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userID := uint(claims["user_id"].(float64))
+
+	var req DiagnosisRequest
+	if err := c.BodyParser(&req); err != nil {
+		return fiber.ErrBadRequest
+	}
+
+	service := services.NewDiagnosisService()
+	diag, err := service.CreateDiagnosis(userID, req.Category, req.Answers)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{
+		"success": true,
+		"data":    diag,
+	})
+}
